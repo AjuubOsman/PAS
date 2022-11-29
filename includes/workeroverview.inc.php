0098f7d75dc04f28ad6717ec19fc429208a76a11
yilma <00312238@rijnijssel.nl>
2022-11-29 15:21:34 +0100

    29-11-2022

---
+++ includes/workeroverview.inc.php
@@ -0,0 +1,33 @@
+<div class="table-responsive">
+    <table class="table table-striped table-hover table-bordered table-light border-secondary">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">naam</th>
+            <th scope="col">email</th>
+            <th scope="col">delete button</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Sit</td>
+            <td>Amet</td>
+            <td>Consectetur</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Adipisicing</td>
+            <td>Elit</td>
+            <td>Sint</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Hic</td>
+            <td>Fugiat</td>
+            <td>Temporibus</td>
+        </tr>
+        </tbody>
+
+    </table>
+</div>
