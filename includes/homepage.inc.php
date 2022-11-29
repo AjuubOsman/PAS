0e593a7c165147ce03c6b51df32402cd69292cfb
yilma <00312238@rijnijssel.nl>
2022-11-29 13:35:31 +0100

    29-11-2022

--- includes/homepage.inc.php
+++ includes/homepage.inc.php
@@ -1,5 +1,17 @@
-<?php
-?>
-
-
-Welcome
+<body >
+<head>
+    <meta charset="UTF-8">
+    <title>PAS</title>
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+<body>
+<main>
+    <div class="container-fluid text-center">
+        <div class="card card-cascade wider reverse">
+            <div class="view view-cascade overlay wow fadeIn">
+                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(1).jpg" class="img-fluid" style="width:100%";>
+                <div class="top">PAS</div>
+            </div>
+    </div>
+</main>
+</body>
