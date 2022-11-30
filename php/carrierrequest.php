c6fdcbb34921536a7a078fe83a399d68ad4c4d7d
Ajuub Osman <00303637@rijnijssel.nl>
2022-11-30 08:38:38 +0100

    test

---
+++ php/carrierrequest.php
@@ -0,0 +1,23 @@
+<?php
+include '../../private/conn.php';
+
+$carrierid = $_POST['carrierid'];
+
+$status = $_POST['action'];
+
+
+if ($status == 'Approve') {
+
+    $stmt = $conn->prepare("UPDATE carrier SET status =  :status  where carrierid = :carrierid");
+    $stmt ->bindParam(':status' , $status);
+    $stmt->bindParam(':carrierid', $carrierid);
+    $stmt->execute();
+
+} else{
+    $stmt = $conn->prepare("UPDATE carrier SET status =  :status  where carrierid = :carrierid");
+    $stmt ->bindParam(':status' , $status);
+    $stmt->bindParam(':carrierid', $carrierid);
+    $stmt->execute();
+
+}
+header('location: ../index.php?page=carrierrequest  ');
