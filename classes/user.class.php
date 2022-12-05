28f6abb8ee3d58c00261563c4e4f8dd8f0261a58
yilma <00312238@rijnijssel.nl>
2022-12-05 16:25:16 +0100

    05-12-2022

---
+++ classes/user.class.php
@@ -0,0 +1,84 @@
+<?php
+
+class user
+{
+    public
+        $firstname,
+        $middlename,
+        $lastname,
+        $dob,
+        $postalcode,
+        $city,
+        $housenumber,
+        $phonenumber,
+        $email,
+        $password;
+
+    public function __construct($firstname, $middlename, $lastname, $dob, $postalcode, $city, $housenumber, $phonenumber, $email, $password, $userid)
+    {
+
+        $this->firstname = $firstname;
+        $this->middlename = $middlename;
+        $this->lastname = $lastname;
+        $this->dob = $dob;
+        $this->postalcode = $postalcode;
+        $this->city = $city;
+        $this->housenumber = $housenumber;
+        $this->phonenumber = $phonenumber;
+        $this->email = $email;
+        $this->password = $password;
+        $this->userid = $userid;
+
+    }
+
+
+    function register($conn,$firstname,$middlename,$lastname,$dob,$postalcode,$city,$housenumber,$phonenumber,$email,$password)
+    {
+        $stmt = $conn->prepare("INSERT INTO user (firstname,middlename,lastname,dob,postalcode,city,housenumber,phonenumber,email,password)
+                        VALUES(:firstname,:middlename,:lastname,:dob,:postalcode,:city,:housenumber,:phonenumber,:email,:password)");
+        $stmt->bindParam(':firstname', $firstname);
+        $stmt->bindParam(':middlename', $middlename);
+        $stmt->bindParam(':lastname', $lastname);
+        $stmt->bindParam(':dob', $dob);
+        $stmt->bindParam(':postalcode', $postalcode);
+        $stmt->bindParam(':city', $city);
+        $stmt->bindParam(':housenumber', $housenumber);
+        $stmt->bindParam(':phonenumber', $phonenumber);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $password);
+        $stmt->execute();
+    }
+
+
+
+    function login($conn, $email, $password)
+    {
+
+        $sql = "SELECT role, userid FROM user WHERE email = :email AND password = :password";
+        $query = $conn->prepare($sql);
+        $query->bindParam(':email', $email);
+        $query->bindParam(':password', $password);
+        $query->setFetchMode(PDO::FETCH_CLASS, 'user');
+        $query->execute();
+
+
+        $sql = "SELECT carrierid, role FROM carrier WHERE email = :email AND password = :password";
+        $query1 = $conn->prepare($sql);
+        $query1->bindParam(':email', $email);
+        $query1->bindParam(':password', $password);
+        $query1->setFetchMode(PDO::FETCH_CLASS, 'user');
+        $query1->execute();
+    }
+}
+
+class loginaccount extends user
+{
+    public function __construct($conn, $email, $password)
+    {
+        parent::login($conn, $email,$password);
+        $this->email = $email;
+        $this->password = $password;
+    }
+}
+
+?>
