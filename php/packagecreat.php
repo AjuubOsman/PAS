c250ce531c9aebb29a460dedebaa4724356b51b2
Ajuub Osman <00303637@rijnijssel.nl>
2023-01-10 10:42:26 +0100

    test

--- php/packagecreat.php
+++ php/packagecreat.php
@@ -1,7 +1,32 @@
 <?php
-$userid = $_POST['userid'];
+include '../../private/conn.php';
 $senderadres = $_POST['senderadres'];
 $weight = $_POST['weight'];
 $receiveradres = $_POST['receiveradres'];
 $contactinformation = $_POST['contactinformation'];
+$lengte = $_POST['lengte'];
+$breedte = $_POST['breedte'];
+$hoogte = $_POST['hoogte'];
+$insuranced = $_POST['insuranced'];
+$rushdelivery = $_POST['rushdelivery'];
+$userid = $_POST['userid'];
+$measurements = $lengte * $breedte * $hoogte;
 
+
+
+//$stmt = $conn->prepare("INSERT INTO package (senderadres,weight,receiveradres,measurements,contactinformation,userid)
+//                        VALUES(:senderadres,:weight,:receiveradres,:measurements,:contactinformation,:userid)");
+//$stmt->bindParam(':senderadres', $senderadres);
+//$stmt->bindParam(':weight', $weight);
+//$stmt->bindParam(':receiveradres', $receiveradres);
+//$stmt->bindParam(':measurements', $measurements);
+//$stmt->bindParam(':contactinformation', $contactinformation);
+//$stmt->bindParam(':userid', $userid);
+//$stmt->execute();
+
+$packageid = $conn->lastInsertId();
+
+if($insuranced == true OR $rushdelivery == true){
+    var_dump($insuranced,$rushdelivery);
+
+}
