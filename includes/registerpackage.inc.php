a5d57bb85c428213d68f85335203bee87ed3db73
Ajuub Osman <00303637@rijnijssel.nl>
2023-01-10 09:29:35 +0100

    test

--- includes/registerpackage.inc.php
+++ includes/registerpackage.inc.php
@@ -1,24 +1,67 @@
+<?php
 
+$userid = $_SESSION['userid'];
+
+?>
 <body>
 <div class="container mt-3">
     <h2>Pakket Aanmelden</h2>
     <form action="php/packagecreat.php" method="POST" enctype="multipart/form-data">
         <div class="mb-3 mt-3">
-            <label>Voornaam:</label>
-            <input type="text" class="form-control" placeholder="Voornaam" name="firstname">
+            <label>Adres Verzender:</label>
+            <input type="text" class="form-control" placeholder="Adres Verzender" name="senderadres">
         </div>
+
         <div class="mb-3 mt-3">
-            <label>Achternaam:</label>
-            <input type="text" class="form-control" placeholder="Achternaam" name="lastname">
+            <label>Lengte:</label>
+
+            <input type="text" name="lengte" placeholder="Lengte"  min="0" required>
+            cm
         </div>
+
         <div class="mb-3 mt-3">
-            <label>Email:</label>
-            <input type="text" class="form-control" placeholder="Email" name="email">
+            <label>Breedte:</label>
+            <input type="text" name="breedte" placeholder="Breedte" min="0" required>
+            cm
         </div>
+
         <div class="mb-3 mt-3">
-            <label>Wachtwoord:</label>
-            <input type="password" class="form-control" placeholder="Wachtwoord" name="password">
+            <label>Hoogte:</label>
+            <input type="text" name="hoogte" placeholder="Hoogte" min="0" required>
+            cm
         </div>
+
+        <div class="mb-3 mt-3">
+            <label>Gewicht:</label>
+            <input type="text" class="form-control" placeholder="Gewicht" name="weight">
+        </div>
+
+        <div class="mb-3 mt-3">
+            <label>Adres Ontvangen:</label>
+            <input type="text" class="form-control" placeholder="Adres Ontvangen" name="receiveradres">
+        </div>
+
+        <div class="mb-3 mt-3">
+            <label>Telefoonnummer of Email</label>
+            <input type="text" class="form-control" placeholder="Contact gegevens" name="contactinformation">
+        </div>
+
+
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="insuranced" value="true" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+                Pakket Verzekeren? (Pakket Verzekeren is 10% extra boven op de gewoonlijke kosten)
+            </label>
+        </div>
+
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="rushdelivery" value="true" id="flexCheckDefault" >
+            <label class="form-check-label" for="flexCheckDefault">
+                Pakket Met Spoed Leveren? (Spoed Levering is 20% extra boven op de gewoonlijke kosten)
+            </label>
+        </div>
+
+<input type="hidden" name="userid" value="<?= $userid?>">
         <button name="submit" type="submit" class="btn btn-success">Voeg toe</button>
     </form>
 </div>
--- php/packagecreat.php
+++ php/packagecreat.php
@@ -1,1 +1,7 @@
 <?php
+$userid = $_POST['userid'];
+$senderadres = $_POST['senderadres'];
+$weight = $_POST['weight'];
+$receiveradres = $_POST['receiveradres'];
+$contactinformation = $_POST['contactinformation'];
+
